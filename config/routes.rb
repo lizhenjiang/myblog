a04032b0f75f7fbd549d38fb97e48867c4001bf6
lizhenjiang <hellolzj123@163.com>
2014-06-05 15:08:04 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Myblog::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   get "home/index"
 
   devise_for :users
-  root to: "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
